50df3194ff1e0868e94ae78b3a8f80e72b8d2e0f
cema-sp <cema.rus@gmail.com>
2014-05-16 16:32:09 +0400

    more code

--- p081/source.rb
+++ p081/source.rb
@@ -1,9 +1,30 @@
-s1 = "Tab symbol: (\t)"
-s2 = "Three backspaces: 123\b\b\b"
-s3 = "Tab symbol again: (\011)"
-s4 = "Sound beep symbol: (\a \007)"
+class String
+	def margin
+		arr = self.split("\n")
+		arr.map!{|x| x.sub!(/\s*\|/,"")}
+		str = arr.join("\n")
+		self.replace(str)
+	end
+end
 
-puts s1
-puts s2
-puts s3
-puts s4
+s = []
+s << "Tab symbol: (\t)"
+s << "Three backspaces: 123\b\b\b"
+s << "Tab symbol again: (\011)"
+s << "Sound beep symbol: (\a \007)"
+s << %q[Just text with %q: " ' \ | /]
+s << %Q_Same text with %Q: " ' \ | /_
+s << <<'EOF'
+Multi-line 
+text
+...
+EOF
+s << <<EOF.margin
+  |Hello there
+  |	<-wow! TAB
+  |\t<-one more
+EOF
+
+s.each do |str|
+	puts str
+end
