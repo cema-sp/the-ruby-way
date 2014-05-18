b8a97d13bfc609cdfbeed3159230db31bca35987
cema-sp <cema.rus@gmail.com>
2014-05-18 23:43:16 +0400

    p 101 added

--- p100/source.rb
+++ p100/source.rb
@@ -8,3 +8,15 @@ joke = "Y2K bug"
 puts joke.rot13
 
 puts "Hello World".rot13.rot13
+
+coded = "hfCghHIE5LAM."
+
+puts "Tell and press \"Enter\", friend!"
+print "Password: "
+password = gets.chop
+
+if password.crypt("hf") == coded
+	puts "Welcome, Master!"
+else
+	puts "Who are you!?"
+end
