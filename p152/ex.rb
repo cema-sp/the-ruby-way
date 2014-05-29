46cf5ce9161339bb7e931bd1d6592acb4f74605c
cema-sp <cema.rus@gmail.com>
2014-05-29 17:37:10 +0400

    new p: 152

---
+++ p152/ex.rb
@@ -0,0 +1,30 @@
+require 'i18n'
+
+class Person
+		
+	def initialize name,age,children_num
+		@name, @age, @children_num = name, age, children_num
+	end
+	
+	def show
+		#puts _("Information")
+		puts I18n.t 'Information'
+		#puts _("Name: %{name}, Age: %{age}") % {name: @name, age: @age}
+		puts I18n.t 'name_age', name: @name, age: @age
+		#puts n_("%{name} has a child.", "%{name} has %{num} children.", @children_num) % {name: @name, num: @children_num}
+		puts I18n.t 'children', count: @children_num, name: @name
+	end
+end
+
+I18n.enforce_available_locales = true
+I18n.load_path = ['i18n/en.yml','i18n/ru.yml']
+
+I18n.locale = :en
+puts "Current locale: #{I18n.locale}"
+john = Person.new("John", 25, 1)
+john.show
+
+I18n.locale = :ru
+puts "Current locale: #{I18n.locale}"
+linda = Person.new("Linda", 30, 3)
+linda.show
