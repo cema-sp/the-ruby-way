dac895219c0fe7449b99b3ba9d27f7e3f6e55a40
cema-sp <cema.rus@gmail.com>
2014-06-13 23:01:47 +0400

    p 264: some chages

--- p264/ex.rb
+++ p264/ex.rb
@@ -10,7 +10,7 @@ class Stack
 	def pop
 		@store.pop
 	end
-	# lst in array is first in stack
+	# last in array is first in stack
 	def peek
 		@store.last
 	end
@@ -18,20 +18,20 @@ class Stack
 		@store.empty?
 	end
 end
-
+# Queue is FIFO
 class Queue
 	def initialize
 		@store=[]
 	end
-	def push(v)
-		@store.unshift(v)
+	def enqueue(v)
+		@store  << v
 	end
-	def pop
-		@store.pop
+	def dequeue
+		@store.shift
 	end
-	# lst in array is first in stack
+	# first in array is first in queue
 	def peek
-		@store.last
+		@store.first
 	end
 	def empty?
 		@store.empty?
@@ -58,6 +58,47 @@ def paren_mach(str)
 	return stack.empty?
 end
 
+def towers(list)
+	states = {}
+	n, src, dst, aux = list.last
+	states[src] = n
+	states[dst] = 0
+	states[aux] = 0
+	while !list.empty?
+		n, src, dst, aux = list.pop
+		if n == 1
+			states[src] -= 1
+			states[dst] += 1
+			print "#{src} -> #{dst} "
+			states.each {|k,v| print "#{k}:#{v} "}
+			puts ''
+		else
+			list.push([n-1, aux, dst, src])
+			list.push([1, src, dst, aux])
+			list.push([n-1, src, aux, dst])
+		end
+	end
+end
+
+def towers_rec(states,list)
+	n, src, dst, aux = *list
+	states ||= states
+	states[src] ||= n
+	states[dst] ||= 0
+	states[aux] ||= 0
+	if n == 1
+		states[src] -= 1
+		states[dst] += 1
+		print "#{src} -> #{dst} "
+		states.each {|k,v| print "#{k}:#{v} "}
+		puts ''
+	else
+		towers_rec(states,[n-1, src, aux, dst])
+		towers_rec(states,[1, src, dst, aux])
+		towers_rec(states,[n-1, aux, dst, src])
+	end
+end
+
 str1 = '((a-b)*2)+5*[c/d]'
 str2 = '((a-b)*2+5*[c/d]'
 
@@ -66,3 +107,9 @@ puts paren_mach(str1) ? "Correct" : "Incorrect"
 puts "String: #{str2}"
 puts paren_mach(str2) ? "Correct" : "Incorrect"
 
+list = [[3, "src", "dst", "aux"]]
+puts "No recursion:"
+towers(list)
+list = [[3, "src", "dst", "aux"]]
+puts "With recursion:"
+towers_rec({},list.flatten)
