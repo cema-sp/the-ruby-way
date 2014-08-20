819e1e9635725efa35a2bf6b3b75ae9d87c95785
cema-sp <cema.rus@gmail.com>
2014-08-20 16:12:25 +0400

    multithread version

--- p564/sr.rb
+++ p564/sr.rb
@@ -4,8 +4,18 @@ PORT = 12321
 
 server = TCPServer.new(PORT)
 
-while (session = server.accept)
-	puts "Recieved packet"
-	session.puts Time.new
-	session.close
+m_thr = Thread.new do
+	while (session = server.accept)
+		Thread.new(session) do |my_session|
+			puts "Recieved packet"
+			session.puts Time.new
+			session.close
+		end
+	end
 end
+
+inp = ""
+while (inp!='q')
+	inp = gets.chomp
+end
+m_thr.kill
